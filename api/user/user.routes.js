84c72cc4430dfa1108a84915dc116d2cae071aed
daniel <daniel.evanenko@gmail.com>
2025-04-27 23:26:11 +0300

    add log to user crud

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { addUser, getUser, getUsers, removeUser, updateUser } from './user.controller.js'
 import { requireAdmin, requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/log.middleware.js'
 
 const router = express.Router()
 
-router.get('/', requireAdmin, getUsers)
-router.get('/:userId', requireAuth, getUser)
-router.put('/:userId', requireAdmin, updateUser)
-router.post('/', addUser)
-router.delete('/:userId', requireAdmin, removeUser)
+router.get('/', log, requireAdmin, getUsers)
+router.get('/:userId', log, requireAuth, getUser)
+router.put('/:userId', log, requireAdmin, updateUser)
+router.post('/', log, addUser)
+router.delete('/:userId', log, requireAdmin, removeUser)
 
 export const userRoutes = router
